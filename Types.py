d8671b9dd1f73c23d4f82628a87054647efa641a
Maryelin <mayeat94@gmail.com>
2022-09-09 17:10:05 -0300

    types

--- Types.py
+++ Types.py
@@ -1,10 +1,10 @@
 # Todos estos valores pertenece a algún tipo
-# para python los tipos son oas denominadas "clases"
+# para python los tipos son las denominadas "clases"
 # estos tres ejemplos son respectivamente
 
-# int: números enteros sin parte fraccionaria
+# int: números enteros son parte fraccionaria
 print(type(2))
 # float: para números con parte fraccionaria
 print(type(7.0))
-# str: parqa las cadenas de caracteres o simplemente texto (string)
+# str: para las cadenas de caracteres o simplemente texto (string)
 print(type("Hola mundo"))
